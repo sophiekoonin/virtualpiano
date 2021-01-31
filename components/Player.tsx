bf365e38b4f5959a249c80038a08645b7d42a046
Sophie Koonin <sophie@localghost.dev>
2021-01-31 18:50:43 +0000

    Clear timeouts when stopping the scale

--- components/Player.tsx
+++ components/Player.tsx
@@ -17,29 +17,39 @@ function generatePianoNotes(numOctaves: number, startingOctave: number) {
 }
 
 export default function Player({ notes }: Props) {
+  const [osc, setOsc] = useState<OscillatorNode>()
   const [currentNote, setCurrentNote] = useState(-1)
   const [pianoNotes] = useState(generatePianoNotes(2, 4))
+  const [noteTimeouts, setNoteTimeouts] = useState([])
   function playScale() {
     const audioCtx = new window.AudioContext()
     const wave = audioCtx.createPeriodicWave(new Float32Array(real), new Float32Array(imag));
     const osc = audioCtx.createOscillator();
+    setOsc(osc)
     osc.setPeriodicWave(wave);
 
     osc.connect(audioCtx.destination);
     osc.start();
     for (let i = 0; i < notes.length; i++) {
       const time = audioCtx.currentTime + (i * 0.5)
-      setTimeout(() => {
+      noteTimeouts.push(setTimeout(() => {
         setCurrentNote(pianoNotes.findIndex(n => n.frequency === notes[i].frequency))
-      }, time * 1000)
+        setNoteTimeouts(noteTimeouts.slice(1))
+      }, time * 1000))
       osc.frequency.setValueAtTime(notes[i].frequency, time)
     }
     osc.stop(audioCtx.currentTime + 4)
   }
 
+  function stop() {
+    osc.stop()
+    noteTimeouts.forEach(t => clearTimeout(t))
+    setCurrentNote(-1)
+  }
   return (
     <>
       <button onClick={() => playScale()} type="button">Play scale</button>
+      <button onClick={() => stop()} type="button">Stop</button>
       <Piano octaves={2} currentNote={currentNote} />
     </>
   )
