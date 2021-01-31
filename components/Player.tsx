b6812ac3ed168ee043a718f260b2fb85169749e9
Hebilicious <xsh4k3@gmail.com>
2021-01-31 17:19:18 +0000

    fix: Use typed array
    This fix the firefox issue

--- components/Player.tsx
+++ components/Player.tsx
@@ -2,26 +2,28 @@ type Props = {
   freqs: Array<number>
 }
 
-
-import { real, imag } from '../utils/wavetable'
+import { real, imag } from "../utils/wavetable"
 export default function Player({ freqs }: Props) {
   function playScale() {
-    debugger
     const audioCtx = new window.AudioContext()
-    const wave = audioCtx.createPeriodicWave(real, imag);
-    const osc = audioCtx.createOscillator();
-    osc.setPeriodicWave(wave);
+    const wave = audioCtx.createPeriodicWave(
+      new Float32Array(real),
+      new Float32Array(imag)
+    )
+    const osc = audioCtx.createOscillator()
+    osc.setPeriodicWave(wave)
 
-    osc.connect(audioCtx.destination);
-    osc.start();
+    osc.connect(audioCtx.destination)
+    osc.start()
     for (let i = 0; i < freqs.length; i++) {
-      osc.frequency.setValueAtTime(freqs[i], audioCtx.currentTime + (i * 0.5))
+      osc.frequency.setValueAtTime(freqs[i], audioCtx.currentTime + i * 0.5)
     }
     osc.stop(audioCtx.currentTime + 4)
   }
 
   return (
-
-    <button onClick={() => playScale()} type="button">Play scale</button>
+    <button onClick={() => playScale()} type="button">
+      Play scale
+    </button>
   )
 }
